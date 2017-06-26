036a385489b01519c0203dac23058bf42f313aba
William O'Mullane <womullan@lsst.org>
2017-06-25 22:39:02 -0500

    test sections

--- makeDMtestmilestones.py
+++ makeDMtestmilestones.py
@@ -6,6 +6,7 @@ import sys
 
 fn = sys.argv[1]
 of = fn.replace(".csv", ".tex")
+tf = open("testsections.tex","w")
 out = open(of, "w")
 
 
@@ -19,12 +20,16 @@ with open(fn, "r") as f:
             c= c+ 1
             desc = row[c]
             desc=re.sub(r'\#', '\\#', desc)
+            desc=re.sub(r'Test report:', '', desc)
             c= c+ 1
             date = row[c]
             c= c+ 1
             txt = row[c]
             txt=re.sub(r'\&', 'and', txt)
             txt=re.sub(r'\#', '\\#', txt)
-
             c= c+ 1
+            note = row[c]
             out.write(tid+" & "+date +" &\n NCSA & \\textbf{"+ desc +"} \n" + txt + "\n \\\\ \hline\n")
+            tf.write("\subsection{"+desc+"\\textbf{("+tid+")}\label{"+tid+"}}\n")
+            tf.write(txt+"\n \\newline")
+            tf.write(note+"\n")
