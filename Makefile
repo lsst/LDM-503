08e173bcc2b099955285c188163cbef61c68a669
John Swinbank <swinbank@lsst.org>
2020-07-30 15:47:38 -0700

    Move LDM-503-specific milestone code to this repo

--- Makefile
+++ Makefile
@@ -6,7 +6,10 @@ all: $(tex) dmtestgantt.tex dmtestmilestones.tex testsections.tex acronyms.tex
 
 clean :
 	latexmk -c
-	rm *.pdf
+	rm -f *.pdf
+	rm -f dmtestmilestones.tex
+	rm -f testsections.tex
+	rm -f dmtestgantt.tex
 	rm -rf $(VENVDIR)
 
 tex=LDM-503.tex DMVcdExample.tex approach.tex body.tex dmtestgantt.tex dmtestmilestones.tex dmvv.tex intro.tex rehearsal.tex reporting.tex roles.tex schedtab.tex schedule.tex scivalidation.tex testsections.tex
@@ -21,20 +24,14 @@ venv: milestones/requirements.txt
 		pip install -r milestones/requirements.txt; \
 	)
 
-dmtestmilestones.tex: milestones/milestones.py venv
+dmtestmilestones.tex testsections.tex &: bin/generate_milestones.py venv
 	( \
 		source $(VENVDIR)/bin/activate; \
-		python3 milestones/milestones.py ldm503 --table-location $@; \
+		PYTHONPATH=milestones python3 bin/generate_milestones.py \
 	)
 
 dmtestgantt.tex: milestones/milestones.py venv
 	( \
 		source $(VENVDIR)/bin/activate; \
-		python3 milestones/milestones.py ldm503 --gantt-location $@; \
-	)
-
-testsections.tex: milestones/milestones.py venv
-		( \
-		source $(VENVDIR)/bin/activate; \
-		python3 milestones/milestones.py ldm503 --text-location $@; \
+		PYTHONPATH=milestones python3 milestones/milestones.py gantt --embedded --output $@ \
 	)
---
+++ bin/generate_milestones.py
@@ -0,0 +1,40 @@
+from io import StringIO
+
+from milestones import (gantt_embedded, escape_latex, write_output,
+                        get_latest_pmcs_path, get_local_data_path,
+                        load_milestones)
+
+def generate_table(milestones):
+    output = StringIO()
+    for ms in sorted([ms for ms in milestones if ms.code.startswith("LDM-503")],
+                     key=lambda x: (x.due, x.code)):
+        output.write(f"{escape_latex(ms.code)} &\n")
+        output.write(f"{escape_latex(ms.due.strftime('%Y-%m-%d'))} &\n")
+        output.write("NCSA &\n")
+        output.write(f"{escape_latex(ms.name)} \\\\\n\n")
+    return output.getvalue()
+
+def generate_commentary(milestones):
+    output = StringIO()
+    for ms in sorted([ms for ms in milestones if ms.code.startswith("LDM-503")],
+                     key=lambda x: (x.due, x.code)):
+        output.write(f"\\subsection{{{escape_latex(ms.name)} (\\textbf{{{escape_latex(ms.code)}}})}}\n")
+        output.write(f"\\label{{{escape_latex(ms.code)}}}\n\n")
+        output.write("\\subsubsection{Execution Procedure}\n\n")
+        if ms.test_spec:
+            output.write(f"This text will be executed following the procedure "
+                         f"defined in {escape_latex(ms.test_spec)}.\n\n")
+        else:
+            output.write("The execution procedure for this test is "
+                         "currently unspecified.\n\n")
+        output.write("\\subsubsection{Description}\n\n")
+        output.write(f"{escape_latex(ms.description)}\n\n")
+        if ms.comment:
+            output.write("\\subsubsection{Comments}\n\n")
+            output.write(f"{escape_latex(ms.comment)}\n\n")
+    return output.getvalue()
+
+if __name__ == "__main__":
+    milestones = load_milestones(get_latest_pmcs_path(), get_local_data_path())
+    write_output("dmtestmilestones.tex", generate_table(milestones))
+    write_output("testsections.tex", generate_commentary(milestones))
